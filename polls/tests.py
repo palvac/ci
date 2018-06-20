1342edf9109371f06dfa5691f4823c77a4a4fc68
Daniel Mbaluka <danielmbaluka@gmail.com>
2018-06-20 17:36:08 +0300

    test status checks reporting to GH

--- polls/tests.py
+++ polls/tests.py
@@ -10,3 +10,6 @@ class TestModels(TestCase):
 
     def test_circle_ci_execution(self):
         self.assertEqual("Circle CI Rocks!", "Circle CI Rocks!")
+
+    def test_status_check_reporting(self):
+        self.assertEqual("Status check Okay!", "Status check Okay!")
