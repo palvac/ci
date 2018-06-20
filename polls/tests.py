11a8e14123f6d5d72331a03df4aec46b592ad0b0
Daniel Mbaluka <danielmbaluka@gmail.com>
2018-06-20 17:22:45 +0300

    add another test

--- polls/tests.py
+++ polls/tests.py
@@ -7,3 +7,6 @@ class TestModels(TestCase):
     def test_poll_created(self):
         Poll.objects.create(name="Subaru Forester SG5 Poll")
         self.assertEqual(Poll.objects.count(), 1)
+
+    def test_circle_ci_execution(self):
+        self.assertEqual("Circle CI Rocks!", "Circle CI Rocks!")
